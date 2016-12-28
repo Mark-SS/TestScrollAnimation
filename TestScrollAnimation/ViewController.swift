b856337b397b1a4a822715ac26f1f8ea7e4815ea
gongliang <gongliang@aprbrother.com>
2016-12-28 16:48:20 +0800

    Initial Commit

---
+++ TestScrollAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestScrollAnimation
+//
+//  Created by gongliang on 2016/12/28.
+//  Copyright © 2016年 GL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
